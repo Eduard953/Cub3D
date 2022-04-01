0d789a7ae4dcfe7f8928d9b4fbf018d2aeec7013
Eduard Beiline <eduardbeiline@Eduards-Air.local>
2022-04-01 14:48:59 +0200

    added complete parsing of the .cub file

---
+++ source/parse_helpers.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_helpers.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebeiline <ebeiline@42wolfsburg.de>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/01 14:27:58 by ebeiline          #+#    #+#             */
+/*   Updated: 2022/04/01 14:45:25 by ebeiline         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	get_color(char *line)
+{
+	int	color;
+
+	color = ft_atoi(line);
+	if (color < 0 || color > 255)
+		error("color value out of rgb range");
+	while (ft_isdigit(*line))
+		line++;
+	line++;
+	return (color);
+}
+
+char	*parse_texture(char *line)
+{
+	char	*texture;
+	int		len;
+
+	texture = ft_strtrim(line, " ");
+	len = ftstrlen(texture);
+	if (len < 5 || ft_strnstr(texture + len - 4, ".xpm", 4) == NULL)
+	{
+		printf("use .xpm texture");
+		exit(1);
+	}
+	return (texture);
+}
+
+int	parse_color(char *line)
+{
+	int	color;
+	int	r;
+	int	g;
+	int	b;
+
+	while (*line == ' ')
+		line++;
+	r = get_color(&line);
+	g = get_color(&line);
+	b = get_color(&line);
+	color = (r << 16) + (g << 8) + b;
+	return (color);
+}
+
+int	parse_line(t_data *data, char *line)
+{
+	int	i;
+
+	i = 0;
+	while (line[i] == ' ')
+		i++;
+	if (line[i] == '\0')
+		return (1);
+	if (line[i] == 'N' && line[i + 1] == 'O')
+		data->map.texture_north = parse_texture(line + i + 2);
+	else if (line[i] == 'S' && line[i + 1] == 'O')
+		data->map.texture_south = parse_texture(line + i + 2);
+	else if (line[i] == 'E' && line[i + 1] == 'A')
+		data->map.texture_east = parse_texture(line + i + 2);
+	else if (line[i] == 'W' && line[i + 1] == 'E')
+		data->map.texture_west = parse_texture(line + i + 2);
+	else if (line[i] == 'F')
+		data->map.floor_color = parse_color(line + i + 1);
+	else if (line[i] == 'C')
+		data->map.ceiling_color = parse_color(line + i + 1);
+	else
+	{
+		get_x_y(data, line, &i);
+		return (0);
+	}
+	return (1);
+}
