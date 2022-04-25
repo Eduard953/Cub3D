dea041ca88ede5075955340fb37baccfdb7e3a1e
ebeiline <ebeiline@students.42wolfsburg.de>
2022-04-25 17:48:22 +0300

    added rotation

--- source/rotate.c
+++ source/rotate.c
@@ -6,9 +6,22 @@
 /*   By: ebeiline <ebeiline@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 13:27:39 by ebeiline          #+#    #+#             */
-/*   Updated: 2022/04/25 13:27:47 by ebeiline         ###   ########.fr       */
+/*   Updated: 2022/04/25 17:46:52 by ebeiline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+void	rotation_left(t_player ash)
+{
+	ash.view_angle -= ROTATION;
+	if (ash.view_angle < 0)
+		ash.view_angle = 360;
+}
+
+void	rotation_right(t_player ash)
+{
+	ash.view_angle += ROTATION;
+	if (ash.view_angle > 360)
+		ash.view_angle = 0;
+}
