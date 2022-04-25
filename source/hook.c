6632c25a7cc37d06beef707302ebd0d198727f3d
ebeiline <ebeiline@students.42wolfsburg.de>
2022-04-25 16:44:13 +0300

    add functions

--- source/hook.c
+++ source/hook.c
@@ -6,7 +6,7 @@
 /*   By: ebeiline <ebeiline@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 16:20:28 by ebeiline          #+#    #+#             */
-/*   Updated: 2022/04/25 13:13:24 by ebeiline         ###   ########.fr       */
+/*   Updated: 2022/04/25 13:26:27 by ebeiline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,17 @@ int	keys(int key, t_data *data)
 {
 	if (key == ESC)
 		red_cross(data);
+	else if (key == W_MAC)
+		move_forward(data);
+	else if (key == S_MAC)
+		move_backward(data);
+	else if (key == A_MAC)
+		move_left(data);
+	else if (key == D_MAC)
+		move_right(data);
+	else if (key == LEFT)
+		rotate_left(data);
+	else if (key == RIGHT)
+		rotate_right(data);
 	return (0);
 }
