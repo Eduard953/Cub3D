2605b534dae263b87a827e27fac16a7e65163d7f
ebeiline <ebeiline@students.42wolfsburg.de>
2022-04-25 14:20:33 +0300

    added mac keycodes

--- source/keys.c
+++ source/keys.c
@@ -6,7 +6,7 @@
 /*   By: ebeiline <ebeiline@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 16:20:28 by ebeiline          #+#    #+#             */
-/*   Updated: 2022/04/10 19:34:59 by pstengl          ###   ########.fr       */
+/*   Updated: 2022/04/25 13:13:24 by ebeiline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	keys(int key, t_data *data)
 {
-	if (key == 65307)
+	if (key == ESC)
 		red_cross(data);
 	return (0);
 }
