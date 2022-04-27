183282658b5085319d6cb110c13d5c269c6a8a39
ebeiline <ebeiline@students.42wolfsburg.de>
2022-04-27 17:39:45 +0300

    position is not updating

--- source/movements.c
+++ source/movements.c
@@ -6,7 +6,7 @@
 /*   By: ebeiline <ebeiline@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 16:20:28 by ebeiline          #+#    #+#             */
-/*   Updated: 2022/04/26 19:18:30 by ebeiline         ###   ########.fr       */
+/*   Updated: 2022/04/27 17:39:11 by ebeiline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ void	move_forward(t_data *data)
 	double	dx;
 	double	dy;
 	
-	dx = cos(degtorad(data->ash.view_angle)) * MOVE_SPEED;
-	dy = sin(degtorad(data->ash.view_angle)) * MOVE_SPEED;
-	if (data->map.tiles[(int)(data->ash.pos.y + dy)][(int)(data->ash.pos.x + dx)] == '0')
+	dy = cos(degtorad(data->ash.view_angle)) * MOVE_SPEED;
+	dx = sin(degtorad(data->ash.view_angle)) * MOVE_SPEED;
+	if (data->map.tiles[(int)(data->ash.pos.y - dy)][(int)(data->ash.pos.x - dx)] == '0')
 	{
-		data->ash.pos.x += dx;
-		data->ash.pos.y += dy;
+		data->ash.pos.x -= dx;
+		data->ash.pos.y -= dy;
 	}
 	printf("Position: %f, %f\n", data->ash.pos.x, data->ash.pos.y);
 }
@@ -33,13 +33,13 @@ void	move_backward(t_data *data)
 	double	dx;
 	double	dy;
 	
-	dx = cos(degtorad(data->ash.view_angle)) * MOVE_SPEED;
-	dy = sin(degtorad(data->ash.view_angle)) * MOVE_SPEED;
+	dy = cos(degtorad(data->ash.view_angle)) * MOVE_SPEED;
+	dx = sin(degtorad(data->ash.view_angle)) * MOVE_SPEED;
 	printf("delta x: %f, delta y: %f\n", dx, dy);
 	if (data->map.tiles[(int)(data->ash.pos.y - dy)][(int)(data->ash.pos.x - dx)] == '0')
 	{
-		data->ash.pos.x -= dy;
-		data->ash.pos.y -= dx;
+		data->ash.pos.x += dx;
+		data->ash.pos.y += dy;
 	}
 	printf("Position: %f, %f\n", data->ash.pos.x, data->ash.pos.y);
 }
@@ -49,10 +49,13 @@ void	move_left(t_data *data)
 	double	dx;
 	double	dy;
 	
-	dx = (degtorad((data->ash.view_angle - 90) % 360)) * MOVE_SPEED;
-	dy = (degtorad((data->ash.view_angle - 90) % 360)) * MOVE_SPEED;
-	data->ash.pos.x -= dx;
-	data->ash.pos.y -= dy;
+	dy = cos(degtorad((data->ash.view_angle - 90) % 360)) * MOVE_SPEED;
+	dx = sin(degtorad((data->ash.view_angle - 90) % 360)) * MOVE_SPEED;
+	if (data->map.tiles[(int)(data->ash.pos.y + dy)][(int)(data->ash.pos.x + dx)] == '0')
+	{
+		data->ash.pos.x += dx;
+		data->ash.pos.y += dy;
+	}
 	printf("Position: %f, %f\n", data->ash.pos.x, data->ash.pos.y);
 }
 
@@ -61,9 +64,12 @@ void   move_right(t_data *data)
 	double	dx;
 	double	dy;
 	
-	dx = cos(degtorad((data->ash.view_angle + 90) % 360)) * MOVE_SPEED;
-	dy = sin(degtorad((data->ash.view_angle + 90) % 360)) * MOVE_SPEED;
-	data->ash.pos.x -= dx;
-	data->ash.pos.y -= dy;
+	dy = cos(degtorad((data->ash.view_angle + 90) % 360)) * MOVE_SPEED;
+	dx = sin(degtorad((data->ash.view_angle + 90) % 360)) * MOVE_SPEED;
+	if (data->map.tiles[(int)(data->ash.pos.y + dy)][(int)(data->ash.pos.x + dx)] == '0')
+	{
+		data->ash.pos.x += dx;
+		data->ash.pos.y += dy;
+	}
 	printf("Position: %f, %f\n", data->ash.pos.x, data->ash.pos.y);
 }
