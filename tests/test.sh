1849000585d0766d033313cc33ac5843d9427f16
Peter Stengl <pstengl@student.42wolfsburg.de>
2022-05-11 17:06:04 +0200

    Add more tests

--- tests/test.sh
+++ tests/test.sh
@@ -4,26 +4,26 @@ if [[ ! -f "$EXE_PATH" ]]; then
     exit 1
 fi
 
-all_tests=0
-fail_test=0
-
 function run_test() {
     mappath="tests/test_maps/$1"
     comment="$2"
+    expect_code="1"
+    if [[ "$3" ]]; then
+        expect_code="$3"
+    fi
     echo
     if [[ "$comment" ]]; then
         echo "Testing $comment"
     fi
     echo "Trying Map path: $mappath"
-    all_test=$(($all_tests + 1))
-    $EXE_PATH $mappath
+    valgrind -q --leak-check=full $EXE_PATH $mappath
     errorcode=$?
     echo
-    if [[ $errorcode != 0 ]]; then
+    if [[ "$errorcode" == "$expect_code" ]]; then
         echo "==PASS=="
     else
         echo "==FAIL=="
-        fail_test=$(($fail_test + 1))
+        exit 1
     fi
 }
 
@@ -34,8 +34,15 @@ echo "RUNNING MAP TESTS:"
 run_test "doesnotexist.cub" "not existing file"
 run_test "wrongend.txt" "wrong file ending"
 run_test "wrongformat.cub" "wrong format in the file"
+run_test "missingtexture.cub" "missing a texture"
+run_test "missingceilcolor.cub" "missing the ceiling color"
+run_test "missingfloorcolor.cub" "missing the floor color"
+run_test "doortooutside.cub" "a door to the outside of the map"
+run_test "notenclosed.cub" "map is not enclosed"
+run_test "spawnnotmap.cub" "spawn is not on map"
+run_test "spawnaswall.cub" "spawn is a wall tile"
+run_test "doublespawn.cub" "2 spawn points"
 
-echo
-echo "Ran $all_test tests"
-echo "$fail_test tests failed"
+run_test "test2.cub" "Correct Map" 0
+run_test "test1.cub" "Correct Map" 0
 
